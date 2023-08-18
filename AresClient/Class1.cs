eb196cbfa9ec56bec31e4a0373e4042d863f8f19
Lukáš Műller <hypous2@gmail.com>
2023-08-18 11:13:17 +0200

    - add readme - add first call - update package version 1.0.1

---
+++ AresClient/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System.Xml.Linq;
+
+namespace AresClient;
+
+public class Class1
+{
+    public void Log(string text)
+    {
+        Console.WriteLine(text);
+    }
+    
+    private const string AresUrl = "https://wwwinfo.mfcr.cz/cgi-bin/ares/darv_std.cgi?ico=";
+    private const string LocalName = "Pocet_zaznamu";
+    
+    /// <summary>
+    /// Method to validate ICO in ARES
+    /// </summary>
+    /// <param name="ico">Identification of subject</param>
+    /// <returns>True if ICO is valid</returns>
+    public async Task<bool> ValidIco(string ico)
+    {
+        try
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var url = AresUrl + ico;
+                var response =  await client.GetAsync(url);
+        
+                XElement xmlParsed = XElement.Parse(await response.Content.ReadAsStringAsync());
+                var count = xmlParsed.Descendants().FirstOrDefault(x => x.Name.LocalName == LocalName)?.Value;
+
+                return count != null && count != "0";
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Something gone wrong: {ex.Message}");
+        }
+
+        return false;
+    }
+    
+}
---
+++ AresClient/README.md
@@ -0,0 +1,22 @@
+# [Your NuGet Package Name]
+
+[![NuGet version](https://img.shields.io/nuget/v/AresClient.svg)](https://www.nuget.org/packages/AresClient)
+[![NuGet downloads](https://img.shields.io/nuget/dt/AresClient.svg)](https://www.nuget.org/packages/AresClient/)
+[![Build Status](https://your-ci-badge-link-here)](https://your-ci-url-here)
+
+Welcome to **AresClient**! This package provides [brief description of what it does].
+
+## 🌟 _Why is it special?_ 🌟  
+[Some compelling reasons why someone should use your package. Maybe it's super fast, or maybe it fills a niche need.]
+
+## 🚀 Getting Started
+
+### Prerequisites
+
+- .NET 7.0 core
+- [Any other prerequisites]
+
+### Installation
+
+```bash
+Install-Package AresClient -Version 1.0.0
