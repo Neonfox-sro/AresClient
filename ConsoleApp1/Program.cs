216c77be858ea86a9946b55b3677752b39ffd649
Lukáš Műller <hypous2@gmail.com>
2023-08-23 12:26:45 +0200

    Add Console App for smart testing

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+var client = new AresClient.AresClient();
+var result = await client.FindCompanyByIco("17338786");
+
+Console.WriteLine(result);
